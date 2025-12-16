4d4ef8730158165cd4602bb542429f1fc7848a2f
Creeper666 <1303850300@qq.com>
2025-12-16 12:59:58 +0800

    why?

--- run/cqupt_helper/cqupt_helper.py
+++ run/cqupt_helper/cqupt_helper.py
@@ -79,12 +79,12 @@ def main(bot: ExtendBot, config):
         headers = HEADER.copy()
         headers["Authorization"] = f"Bearer {token}"
         # Use form data as requested: body content stu_num=...
-        data = f"stu_num={target_stu_id}"
+        data = f"stu_num={target_stu_id}".encode("utf-8")
         logger.info(f"Get course table request: {data}")
         async with httpx.AsyncClient() as client:
             response = await client.post(url, headers=headers, content=data)
             # error_msg = traceback.format_exc()
-            # logger.info(f"Get course table response: {response.status_code}, {response.text}")
+            logger.info(f"Get course table response: {response.status_code}, {response.text}")
             response.raise_for_status()
             return response.json()["data"]
 
